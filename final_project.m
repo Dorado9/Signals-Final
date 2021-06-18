d01da4b601b7dd61f522e87646c3b988f2386967
Avraham <avi.hrinevitzky@gmail.com>
2021-06-18 17:38:01 +0300

    all question 1

--- final_project.m
+++ final_project.m
@@ -9,7 +9,7 @@ stem(n,h_n);
 title("The Pulse Response of the LTI System");
 
 % part a qustion e 
-syms h(n) H(z) x y  ;
+syms h(n) H(z)  ;
 u_n = heaviside(n);
 u_n_2 = heaviside(n-2);
 h(n) = ((1/2)^n)*u_n + ((3/4)^n)*u_n_2 ;
@@ -19,10 +19,16 @@ disp(H(z));
 
 
 %part a qustion f 
-%we can find the zeros and poles from the H_z we found in the last qustion
-%and with the function zplan we will plost the into the unite circle 
-H_p_z;
-pzmap(H_p_z)
-r = roots(H(z));
-
+%we can find the zeros and poles of H(z) with finding the roots of the
+%numerator and denominator
+%and with the function zplan we will plot the zeros and poles into the unite circle
+syms z
+eqn_z = 32*z^3 - 24*(z^2)+18*z-9 ; % numerator polynomial that we got from qeustion 1e
+eqn_p = 4*z*((2*z-1)*(4*z-3)) ;% denominator polynomial that we got from qeustion 1e
+z = roots([32 -24 18 -9]);% calculates the roots of the numerator
+p = roots([32 -40 12 0]); %calculates the roots of the denominator
+figure(2)
+zplane(z,p);
+legend('zeros' ,'poles')
+grid
 
